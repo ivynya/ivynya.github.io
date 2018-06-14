1be59107583aaaa287f5bf6d55efb4f307068885
SDBagel <35588499+SDBagel@users.noreply.github.com>
2018-06-14 01:46:40 -0700

    Added JS and JQuery

---
+++ js/index.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+  $("#showfeatured").click(function() {
+    $("#showfeatured").slideUp();
+    $("#showfeatured").promise().done(function() {
+      $("#featured").slideDown();
+    });
+  });
+
+  $("#hidefeatured").click(function() {
+    $("#featured").slideUp();
+    $("#featured").promise().done(function() {
+      $("#showfeatured").slideDown();
+    });
+  });
+  
+});
