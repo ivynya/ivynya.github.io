ee2aaf9bc92b862b96bc82aff8d5dbf3419722df
Kyle <35588499+SDBagel@users.noreply.github.com>
2020-07-04 20:19:56 -0700

    Secure mail form

--- js/site.js
+++ js/site.js
@@ -1,6 +1,15 @@
+// Swaps theme - home page only
 function themeSwap() {
   if (document.documentElement.getAttribute("data-theme") === "sakura")
     document.documentElement.setAttribute("data-theme", "");
   else
     document.documentElement.setAttribute("data-theme", "sakura");
+}
+
+// Gets contact form info, links with a mailto
+function contacter(e) {
+  let subj = document.getElementById("contact_subject").value;
+  let body = document.getElementById("contact_message").value;
+  window.location.href = "mailto:sdbagel@gmail.com?subject="+subj+"&body="+body;
+  e.preventDefault();
 }
