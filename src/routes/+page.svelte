ab3b5347eec6ac663d80069c73cd4175c81bea8a
ivynya <ivy@ivy.direct>
2023-12-17 15:07:50 -0800

    repo: page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,14 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+
+</script>
+
+<main>
+  <h1>Page</h1>
+  <p>Page content</p>
+</main>
+
+<style lang="scss">
+  main {
+    padding: 1rem;
+  }
+</style>
